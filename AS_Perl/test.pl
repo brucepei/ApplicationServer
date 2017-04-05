577eff38d94fb78e37116b00cbf0e9d4e0e12f96
brucepei <brucepei@hotmail.com>
2017-04-05 23:13:22 +0800

    verify inet pipe can use in the same thread, but POE wheel will forbid it

--- AS_Perl/test.pl
+++ AS_Perl/test.pl
@@ -9,6 +9,7 @@ use POE::Wheel;
 use POE::Component::Pool::Thread;
 use Logger;
 use JSON;
+use IO::Pipely qw(pipely);
 
 Logger->new(path=>'log/as.log', append => 0);
 
@@ -18,56 +19,9 @@ sub get_logger {
 }
 
 
-sub run_received_command {
-    my ($sender, $args_json) = @_;
-    my $log = get_logger();
-    my ($args, $json_result);
-    if( 1 ) {
-        $log->debug("Run command: '$args_json'!");
-        $json_result = encode_json( {stdout => undef, stderr => $@, exit => -1} );
-    } else {
-        $log->debug("Begin to run command: $args->{command} from session $sender!");
-        $json_result = encode_json( {stdout => undef, stderr => undef, exit => 0} );
-        $log->debug("End to run command: $args->{command} from session $sender!");
-    }
-    return ($sender, $json_result);
-}
-
-
-sub on_complete_command {
-    my ($kernel, $sender, $json_result) = @_[ KERNEL, ARG0, ARG1 ];
-    my $log = get_logger();
-    $log->debug("Cet command result for session $sender: '$json_result'!");
-}
-
-POE::Session->create(
-    inline_states => {
-        _start => sub {
-            $_[HEAP]->{thread_pool} = POE::Component::Pool::Thread->new(
-                MinFree       => 2,
-                MaxFree       => 5,
-                MaxThreads    => 15,
-                StartThreads  => 2,
-                Name          => "thread_pool",
-                EntryPoint    => \&run_received_command,
-                CallBack      => \&on_complete_command,
-            );
-            $_[KERNEL]->delay(alive => 1, 0)
-        },
-        alive => sub {
-            my $counter = $_[ARG0];
-            $counter++;
-            print "hello $counter\n";
-            # if ($counter < 10 || $counter > 140) {
-                # $_[KERNEL]->post(thread_pool => run => $_[SESSION]->ID, $counter);
-            # }
-            # else {
-                # warn "counter remains: " . $counter;
-            # }
-            $_[KERNEL]->delay( alive => 1, $counter );
-        },
-    },
-);
-
-POE::Kernel->run();
-exit;
+my ($pipe_in, $pipe_out) = pipely(debug => 1);
+$pipe_out->print( ": alive" );
+$pipe_out->flush;
+sysread($pipe_in, my $buffer = '', 7);
+warn "input: [$buffer]";
+<STDIN>;
